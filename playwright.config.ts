7a3032f13610a063ef8cdcf790c505ac5852ee60
supriya <supriyaq96@gmail.com>
2026-02-14 11:10:35 +0530

    update the playwright.config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,
 
 
 //own workers on local enviornment--->added by supriya
@@ -52,7 +52,7 @@ export default defineConfig({
 
 
   /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
 
